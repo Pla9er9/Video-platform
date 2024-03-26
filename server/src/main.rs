705dfbcf672715baf39a31f0ca84d919ebd902fc
Pla9er9 <drugiekonto_99@o2.pl>
2024-03-26 16:11:11 +0100

    add: rocket framework and few empty routes

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/login")]
+fn login() -> &'static str {
+    "Login"
+}
+
+#[post("/register")]
+fn register() -> &'static str {
+    "Registration"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![login])
+        .mount("/", routes![register])
 }
