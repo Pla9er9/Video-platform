b5c75173c2ab7d550f607e0246ad17c55db4fa91
Pla9er <drugiekonto_99@o2.pl>
2024-04-02 22:31:56 +0200

    change: navbar for authenticated users and for not authenticated

--- app/src/components/Menu.tsx
+++ app/src/components/Menu.tsx
@@ -1,11 +1,19 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '@/lib/store';
 import './Menu.scss'
 
 export default function Menu() {
+    const token = useSelector((state: RootState) => state.token.value);
+    
     return (
         <div id="menu" className='column'>
             <a href="/">Home</a>
-            <a href="/login">Login</a>
-            <a href="/registration">Sign up</a>
+            {token ? <>
+                <a href="/upload">Upload video</a>
+            </> : <>
+                <a href="/login">Login</a>
+                <a href="/registration">Sign up</a>
+            </>}
         </div>
     )
 }
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import MenuButton from "./MenuButton";
-import './Navbar.scss'
+import "./Navbar.scss";
+import { RootState } from "@/lib/store";
+import { useSelector } from "react-redux";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Navbar({ onClick }) {
+    const data = useSelector((state: RootState) => state.token);
+
     return (
         <nav className="row">
             <a href="/">Video platform</a>
             <MenuButton onClick={onClick} />
+            {data.value ? (
+                <Avatar style={{marginLeft: "8px", width: "35px", height: "35px"}}>
+                    <AvatarImage
+                        src=""
+                        alt="@shadcn"
+                    />
+                    <AvatarFallback style={{fontSize: "12px"}}>{data.username?.slice(0, 2)}</AvatarFallback>
+                </Avatar>
+            ) : (
+                <></>
+            )}
         </nav>
     );
 }
