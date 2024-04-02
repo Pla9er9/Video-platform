d18024e00fc6a6ebf4c27c31d33ffdf6d71cd92a
Pla9er <drugiekonto_99@o2.pl>
2024-04-02 23:20:53 +0200

    add: urls from video app and delete old test url

--- server/video_platform/urls.py
+++ server/video_platform/urls.py
@@ -1,8 +1,14 @@
 from django.urls import re_path
 from . import views
+import video.views as video
+from django.urls import path
 
 urlpatterns = [
     re_path('login', views.login),
     re_path('signup', views.signup),
-    re_path('test_token', views.test_token)
+    re_path('video/new', video.createVideo),
+    path("video/<uuid:id>", video.getVideoData),
+    path("video/<uuid:id>/upload/video", video.uploadVideo),
+    path("video/<uuid:id>/upload/miniature", video.uploadMiniature),
+    path("video/<uuid:id>/miniature", video.getMiniature),
 ]
