e24f00b34b65b36ca50e4ea76e985be921a3b234
Pla9er <drugiekonto_99@o2.pl>
2024-04-02 21:34:44 +0200

    add: not found page

---
+++ app/src/app/404/page.tsx
@@ -0,0 +1,10 @@
+import FormTittle from "@/components/FormTittle";
+import Main from "@/components/Main";
+
+export default function NotFound() {
+    return (
+        <Main>
+            <FormTittle text="Not found" className="text-7xl" />
+        </Main>
+    )
+}
