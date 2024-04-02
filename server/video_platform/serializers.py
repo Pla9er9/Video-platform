08c41b464e9b4c746f69a4816e8c092e6224913e
Pla9er <drugiekonto_99@o2.pl>
2024-04-02 23:19:35 +0200

    add: video serializer

--- server/video_platform/serializers.py
+++ server/video_platform/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from user.models import UserProfile
+from video.models import Video
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = UserProfile
         fields = ['id', 'username', 'password', 'email', 'first_name']
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = Video
+        fields = ['title', 'description', 'isPrivate']
