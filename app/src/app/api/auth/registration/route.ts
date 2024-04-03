4bc3f371d88eae71e44c5e9a3ea570087d1d27cb
Pla9er <drugiekonto_99@o2.pl>
2024-04-03 22:48:02 +0200

    fix: login and registration

--- app/src/app/api/auth/registration/route.ts
+++ app/src/app/api/auth/registration/route.ts
@@ -1,21 +1,26 @@
-export async function POST(request: Request) {
-    const data = await request.json()
+import { cookies } from "next/headers";
 
-    const res = await fetch('http://127.0.0.1:8000/signup', {
+export async function POST(request: Request) {
+    const data = await request.json();
+    const cookieStore = cookies();
+
+    const res = await fetch("http://127.0.0.1:8000/signup", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
             "Content-Type": "application/json",
-          },
-    })
+        },
+    });
 
-    const json = await res.json()
-
-    if (!json.ok) {
+    const json = await res.json();
+    if (!res.ok) {
         return Response.json(json, {
-            status: res.status
-        })
+            status: res.status,
+        });
     }
 
-    return Response.json({ 'token': json.token })
+    cookieStore.set("jwtToken", json.token, { secure: true, httpOnly: true });
+    cookieStore.set("username", json.user.username);
+
+    return Response.json({ token: json.token });
 }
