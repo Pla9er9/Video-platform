5e44dd233abaf0fa94e970317e366d9ab24ed7bf
Pla9er <drugiekonto_99@o2.pl>
2024-04-05 23:13:58 +0200

    add: link to profile in navbar

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import "./Navbar.scss";
 import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 export default function Navbar({ onClick }) {
     const data = useSelector((state: RootState) => state.token);
@@ -14,13 +15,15 @@ export default function Navbar({ onClick }) {
             <a href="/">Video platform</a>
             <MenuButton onClick={onClick} />
             {data.value ? (
-                <Avatar style={{marginLeft: "8px", width: "35px", height: "35px"}}>
-                    <AvatarImage
-                        src=""
-                        alt="@shadcn"
-                    />
-                    <AvatarFallback style={{fontSize: "12px"}}>{data.username?.slice(0, 2)}</AvatarFallback>
-                </Avatar>
+                <Link href={`/@${data.username}`} style={{margin: "0 8px"}}>
+                    <Avatar style={{width: "35px", height: "35px"}}>
+                        <AvatarImage
+                            src=""
+                            alt="@shadcn"
+                        />
+                        <AvatarFallback style={{fontSize: "12px"}}>{data.username?.slice(0, 2)}</AvatarFallback>
+                    </Avatar>
+                </Link>
             ) : (
                 <></>
             )}
