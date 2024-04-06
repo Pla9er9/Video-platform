78078eac75e5ef954f99a4eb34f479c80c299174
Pla9er <drugiekonto_99@o2.pl>
2024-04-06 15:16:54 +0200

    update: userprofile component

--- server/user/models.py
+++ server/user/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class UserProfile(AbstractUser):
+    subscriptions = models.IntegerField(default=0)
+    description = models.TextField(default="")
 
     class Meta:
         ordering = ['date_joined']
