f9221d1eeac9ca67af1c601b9038fb0ddd7521a7
Pla9er <drugiekonto_99@o2.pl>
2024-04-26 19:46:53 +0200

    add: auto creating folder structure function

--- server/video/apps.py
+++ server/video/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class VideoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'video'
+
+    def ready(self):
+        import os
+
+        rootMediaDir = './media'
+        paths = ['', '/avatars', '/miniatures', '/videos']
+
+        for p in paths:
+            fullPath = rootMediaDir + p
+            if not os.path.exists(fullPath):
+                os.mkdir(fullPath)
+        
